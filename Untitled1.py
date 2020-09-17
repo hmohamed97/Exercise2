e68d278f685ed11478bba400f70e0dc538a299bf
hmohamed97 <55402813+hmohamed97@users.noreply.github.com>
2020-09-17 17:09:37 -0400

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,30 @@
+
+def freq(str): 
+  
+    # break the string into list of words  
+    str = str.split()          
+    str2 = [] 
+  
+    # loop till string values present in list str 
+    for i in str:              
+  
+        # checking for the duplicacy 
+        if i not in str2: 
+  
+            # insert value in str2 
+            str2.append(i)  
+              
+    for i in range(0, len(str2)): 
+  
+        # count the frequency of each word(present  
+        # in str2) in str and print 
+        #print('Frequency of', str2[i], 'is :', str.count(str2[i]))     
+        print(str2[i],str.count(str2[i]))
+  
+def main(): 
+    str = input("Please enter a string: ")
+    freq(str)                     
+  
+if __name__=="__main__": 
+    main()             # call main function 
+
